3706eff27f3601eb51fe029ec45e38022661c981
Tigran Petrosyan <tigr-080807@mail.ru>
2025-11-15 22:46:30 +0500

    Бреееед

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,15 +2,12 @@ const modal = document.querySelector("#footer__modal");
 const openButton = document.querySelector(".save__button");
 const closeButton = document.querySelector(".modal_button");
 
-const openModal = (e) => {
+openButton.addEventListener("click", (e) => {
   e.preventDefault();
   modal.showModal();
-};
+});
 
-const closeModal = (e) => {
+closeButton.addEventListener("click", (e) => {
   e.preventDefault();
   modal.close();
-};
-
-openButton.addEventListener("click", openModal);
-closeButton.addEventListener("click", closeModal);
+});
